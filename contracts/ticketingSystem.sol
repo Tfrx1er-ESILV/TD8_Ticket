7ba75e76e5b2747db72081b03062809e5127b01a
Théophile Freixa <theophilefreixa@MacBook-Pro-de-Theophile.local>
2020-03-20 14:56:40 +0100

    ✅ 2nd Test succeed
    - add venue
    - add createVenue
    - add modifyVenue

--- contracts/ticketingSystem.sol
+++ contracts/ticketingSystem.sol
@@ -12,18 +12,18 @@ contract artistList
         address payable owner; 
     }
     //Create the pointer
-    uint pointer = 1;
+    uint pointerArtist = 1;
     //List of all artist profiles
     mapping(uint => artistProfil) public artistsRegister;
     //Create an artist
     function createArtist(bytes32 _name, uint _type) public {
-        artistProfil storage newArtist = artistsRegister[pointer];
+        artistProfil storage newArtist = artistsRegister[pointerArtist];
         newArtist.name = _name;
         newArtist.artistCategory = _type;
         newArtist.totalTicketsSold = 0;
         newArtist.owner = msg.sender;
         //We increment the pointer
-        pointer= pointer +1;
+        pointerArtist= pointerArtist +1;
     }
     //Modify an artist
     function modifyArtist(uint _artistId, bytes32 _name, uint _artistCategory, address payable _newOwner) public {
@@ -32,14 +32,54 @@ contract artistList
             msg.sender == artistsRegister[_artistId].owner,
             "You are not allowed to modify the artist"
         );
-        //Make the modifications
+        //Apply the modifications
         artistsRegister[_artistId].name = _name;
         artistsRegister[_artistId].artistCategory = _artistCategory;
         artistsRegister[_artistId].owner = _newOwner;
     }
 }
+//This contract contain all the venue containers
+contract venueList
+{
+    struct venueProfil {
+        //venue parameters
+        bytes32 name;
+        uint capacity;
+        uint standardComission;
+        //Creator
+        //This will allow us to prevent someone from changing another person's venue
+        address payable owner; 
+    }
+    //Create the pointer
+    uint pointerVenue = 1;
+    //List of all artist profiles
+    mapping(uint => venueProfil) public venuesRegister;
+    //Create a venue
+    function createVenue(bytes32 _name, uint _capacity, uint _standardComission) public{
+        venueProfil storage newVenue = venuesRegister[pointerVenue];
+        newVenue.name =_name;
+        newVenue.capacity =_capacity;
+        newVenue.standardComission =_standardComission;
+        newVenue.owner  = msg.sender;
+        //We increment the pointer
+        pointerVenue= pointerVenue +1;
+    }
+    //Modify a Venue
+    function modifyVenue(uint _venueId, bytes32 _name, uint _capacity, uint _standardComission, address payable _newOwner) public {
+        //Just allow the owner
+        require(
+            msg.sender == venuesRegister[_venueId].owner,
+            "You are not allowed to modify the venue"
+        );
+        //Apply the modifications
+        venuesRegister[_venueId].name = _name;
+        venuesRegister[_venueId].capacity = _capacity;
+        venuesRegister[_venueId].standardComission = _standardComission;
+        venuesRegister[_venueId].owner = _newOwner;
+    }
+}
 //This is the main contract
-contract ticketingSystem is artistList
+contract ticketingSystem is artistList, venueList
 {
     constructor () public{}
 }
--- test/01_creatingArtistProfile.js
+++ test/01_creatingArtistProfile.js
@@ -38,30 +38,30 @@ contract('Creating artist profile', function (accounts) {
     assert.equal(retrievedArtist2Info.owner,accounts[1])
 
     })
-//    it('Modifying an artist profile', async function (){
-//    artist1Name = "Electric Octopus"
-//    artist2Name = "David Bowie"
-//    artistCategory = 1
-//    newArtistCategory = 2;
-//
-//    // Creating a new artist and checking it happened correctly
-//    await TicketingSystemInstance.createArtist(web3.utils.fromAscii(artist1Name), artistCategory, {from: accounts[0]}); 
-//    retrievedArtist1Info = await TicketingSystemInstance.artistsRegister(1)
-//    assert.equal(web3.utils.toUtf8(retrievedArtist1Info.name),artist1Name)
-//    
-//    // Trying to modify artists with an account different than owner
-//    //function modifyArtist(uint _artistId, bytes32 _name, uint _artistCategory, address payable _newOwner) 
-//    await tryCatch(TicketingSystemInstance.modifyArtist(1, web3.utils.fromAscii(artist2Name), newArtistCategory, accounts[2], {from: accounts[2]} ), errTypes.revert);
-//    
-//    // Modifying artist profile
-//    await TicketingSystemInstance.modifyArtist(1, web3.utils.fromAscii(artist2Name), newArtistCategory, accounts[2] )
-//
-//    // Checking modification were registered
-//    retrievedArtist2Info = await TicketingSystemInstance.artistsRegister(1);
-//    assert.equal(web3.utils.toUtf8(retrievedArtist2Info.name),artist2Name)
-//    assert.equal(retrievedArtist2Info.artistCategory,newArtistCategory)
-//    assert.equal(retrievedArtist2Info.owner,accounts[2])
-//    })
+    it('Modifying an artist profile', async function (){
+    artist1Name = "Electric Octopus"
+    artist2Name = "David Bowie"
+    artistCategory = 1
+    newArtistCategory = 2;
+
+    // Creating a new artist and checking it happened correctly
+    await TicketingSystemInstance.createArtist(web3.utils.fromAscii(artist1Name), artistCategory, {from: accounts[0]}); 
+    retrievedArtist1Info = await TicketingSystemInstance.artistsRegister(1)
+    assert.equal(web3.utils.toUtf8(retrievedArtist1Info.name),artist1Name)
+    
+    // Trying to modify artists with an account different than owner
+    //function modifyArtist(uint _artistId, bytes32 _name, uint _artistCategory, address payable _newOwner) 
+    await tryCatch(TicketingSystemInstance.modifyArtist(1, web3.utils.fromAscii(artist2Name), newArtistCategory, accounts[2], {from: accounts[2]} ), errTypes.revert);
+    
+    // Modifying artist profile
+    await TicketingSystemInstance.modifyArtist(1, web3.utils.fromAscii(artist2Name), newArtistCategory, accounts[2] )
+
+    // Checking modification were registered
+    retrievedArtist2Info = await TicketingSystemInstance.artistsRegister(1);
+    assert.equal(web3.utils.toUtf8(retrievedArtist2Info.name),artist2Name)
+    assert.equal(retrievedArtist2Info.artistCategory,newArtistCategory)
+    assert.equal(retrievedArtist2Info.owner,accounts[2])
+    })
 
 
 })
